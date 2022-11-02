5ef617039c9ff1b4c5943952ef651fff0f6dd5c2
BuildTools <unconfigured@null.spigotmc.org>
2022-11-02 11:07:55 +0100

    changed setup

--- Lega.Monogame.Shared.Perio/Game1.cs
+++ Lega.Monogame.Shared.Perio/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Lega.Core.Memory;
+﻿using Calendula.Framework.Input;
+using Lega.Core.Memory;
 using Lega.Core.Monogame.Graphics;
 using Lega.Core.Monogame.Input;
 using Lega.Monogame.Shared.Perio.Core;
@@ -7,6 +8,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Lega.Monogame.Shared.Perio
 {
@@ -20,13 +22,29 @@ namespace Lega.Monogame.Shared.Perio
         private VirtualKeyboard _keyboard;
 
         private Texture2D _displayOutput;
+        private RenderTarget2D _target;
         private int _displayOutputScale = 2;
 
         private SpriteFont _font;
 
         private Stopwatch _clock;
         private Stopwatch _debugUpdate;
+        private Stopwatch _debugDraw;
         private bool _clockCycle = false;
+        private bool _back = false;
+
+        public bool IsFullScreen
+        {
+            get => _graphics.IsFullScreen;
+            set
+            {
+                _graphics.IsFullScreen = value;
+                _graphics.ApplyChanges();
+            }
+        }
+
+
+        private Rectangle _destination;
 
         private Random _random = new Random();
 
@@ -40,7 +58,20 @@ namespace Lega.Monogame.Shared.Perio
         protected override void Initialize()
         {
 
-            Window.AllowUserResizing = true;
+            _graphics.PreferredBackBufferWidth = 256 * 4;
+            _graphics.PreferredBackBufferHeight = 192 * 4;
+            _graphics.PreferredBackBufferFormat = SurfaceFormat.Color;
+            _graphics.PreferredDepthStencilFormat = DepthFormat.Depth24; // <-- set depth here
+
+            _graphics.HardwareModeSwitch = false;
+            _graphics.PreferMultiSampling = false;
+            _graphics.IsFullScreen = false;
+            _graphics.SynchronizeWithVerticalRetrace = false;
+            _graphics.ApplyChanges();
+
+            TargetElapsedTime = TimeSpan.FromSeconds(1 / 30f);
+            IsFixedTimeStep = true;
+
 
             _memory = new VirtualMemory(16_384);
             _keyboard = new VirtualKeyboard();
@@ -50,18 +81,30 @@ namespace Lega.Monogame.Shared.Perio
             {
                 //_keyboard.Map(_memory, 0, 16);
                 _display.Map(_memory, 0, _display.BytesPerFrame);
-            }catch (Exception e)
+            }
+            catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
 
             _clock = new Stopwatch();
             _debugUpdate = new Stopwatch();
+            _debugDraw = new Stopwatch();
 
             _display.MemoryRegion.Poke(0, _display.BytesPerFrame, 0x00);
             _display.MemoryRegion.Poke(0, 0x1F);
 
+            Window.AllowUserResizing = true;
+            Window.ClientSizeChanged += OnResize;
+
+            PerformScreenFit(_display.Width, _display.Height);
+
             base.Initialize();
+        }
+
+        private void OnResize(object sender, EventArgs e)
+        {
+            PerformScreenFit(_display.Width, _display.Height);
         }
 
         protected override void LoadContent()
@@ -69,12 +112,19 @@ namespace Lega.Monogame.Shared.Perio
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             _font = Content.Load<SpriteFont>("Default");
             _displayOutput = new Texture2D(GraphicsDevice, _display.Width, _display.Height);
+            _target = new RenderTarget2D(GraphicsDevice, _display.Width, _display.Height);
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            HxKeyboard.Update(gameTime);
+            if (HxKeyboard.IsKeyDown(Keys.LeftAlt))
+            {
+                if (HxKeyboard.IsKeyDownOnce(Keys.Enter))
+                {
+                    IsFullScreen = !IsFullScreen;
+                }
+            }
 
             if (!_clock.IsRunning)
             {
@@ -83,29 +133,45 @@ namespace Lega.Monogame.Shared.Perio
 
 
             _clockCycle = false;
-            if (_clock.Elapsed.TotalMilliseconds >= 1000f / 10)
+            if (_clock.Elapsed.TotalMilliseconds >= 1000f / 30)
             {
                 _debugUpdate.Reset();
                 _debugUpdate.Start();
                 //_keyboard.Update(gameTime);
                 var index = _random.Next(0, _display.MemoryLength);
                 _display.MemoryRegion.Poke(index, (byte)_random.Next(0, 16));
-                _displayOutput.SetData(Util.FromBufferPerio(_display.MemoryRegion.Data.ToArray()));
+
+                new Task(() =>
+                {
+                    _displayOutput.SetData(Util.FromBufferPerio(_display.MemoryRegion.Data.ToArray()));
+                }).Start();
+
                 _debugUpdate.Stop();
-                _clockCycle = true;
+                //_clockCycle = true;
                 _clock.Restart();
             }
-            Window.Title = $"{_debugUpdate.Elapsed.TotalMilliseconds}ms";
+            Window.Title = $"{_debugUpdate.Elapsed.TotalMilliseconds}ms | {_debugDraw.Elapsed.TotalMilliseconds}ms";
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
+            _debugDraw.Reset();
+            _debugDraw.Start();
+            GraphicsDevice.SetRenderTarget(_target);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_displayOutput, Vector2.Zero, Color.White);
+            _spriteBatch.End();
+            GraphicsDevice.SetRenderTarget(null);
+
             GraphicsDevice.Clear(Color.Black);
-
             _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            _spriteBatch.Draw(_target, _destination, Color.White);
+            _spriteBatch.End();
 
-            var x = _displayOutput.Width * _displayOutputScale;
+            /*GraphicsDevice.Clear(Color.Black);
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            var x = _displayOutput0.Width * _displayOutputScale;
             var y = 0;
             var perRow = 32;
             for (int i = 1; i <= _memory.Capacity; i++)
@@ -113,7 +179,7 @@ namespace Lega.Monogame.Shared.Perio
                 _spriteBatch.DrawString(_font, $"{_memory.Peek(i - 1):X2}", new Vector2(x, y), _clockCycle ? Color.Red : Color.White);
                 if (i % perRow == 0)
                 {
-                    x = _displayOutput.Width * _displayOutputScale;
+                    x = _displayOutput0.Width * _displayOutputScale;
                     y += _font.LineSpacing;
                 }
                 else
@@ -122,11 +188,38 @@ namespace Lega.Monogame.Shared.Perio
                 }
                 
             }
-            _spriteBatch.Draw(_displayOutput, new Rectangle(0, 0, _displayOutput.Width * _displayOutputScale, _displayOutput.Height * _displayOutputScale), Color.White);
+            _spriteBatch.Draw(_target, new Rectangle(0, 0, _target.Width * _displayOutputScale, _target.Height * _displayOutputScale), Color.White);
 
-            _spriteBatch.End();
+            _spriteBatch.End();*/
 
             base.Draw(gameTime);
+            _debugDraw.Stop();
         }
+
+        private void PerformScreenFit(int scrW, int scrH)
+        {
+            var (_, _, width, height) = GraphicsDevice.Viewport.Bounds;
+            var aspectRatioViewport = GraphicsDevice.Viewport.AspectRatio;
+            var aspectRatioVirtualDisplay = (float)scrW / scrH;
+
+            var rx = 0f;
+            var ry = 0f;
+            float rw = width;
+            float rh = height;
+
+            if (aspectRatioViewport > aspectRatioVirtualDisplay)
+            {
+                rw = rh * aspectRatioVirtualDisplay;
+                rx = (width - rw) / 2f;
+            }
+            else if (aspectRatioViewport < aspectRatioVirtualDisplay)
+            {
+                rh = rw / aspectRatioVirtualDisplay;
+                ry = (height - rh) / 2f;
+            }
+
+            _destination = new Rectangle((int)rx, (int)ry, (int)rw, (int)rh);
+        }
+
     }
 }
