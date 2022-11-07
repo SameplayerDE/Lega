d72a7ed3005444d65587d2bfdcbfc6e7825c8c16
SameplayerDE <asameplayer@gmail.com>
2022-11-07 19:09:49 +0100

    push

--- Lega.Monogame.OpenGL.Hoopfe/Program.cs
+++ Lega.Monogame.OpenGL.Hoopfe/Program.cs
@@ -2,6 +2,34 @@
 using NAudio.Wave;
 using System.Threading;
 using System;
+using System.Threading.Tasks;
+
+Task.Run(() =>
+{
+    var beep = new SignalGenerator()
+    {
+        Gain = 0.1,
+        Frequency = 100,
+        Type = SignalGeneratorType.SawTooth
+    }.Take(TimeSpan.FromSeconds(5));
+
+    var boop = new SignalGenerator()
+    {
+        Gain = 0.1,
+        Frequency = 100,
+        Type = SignalGeneratorType.SawTooth
+    }.Take(TimeSpan.FromSeconds(5));
+
+    using (var wo = new WaveOutEvent())
+    {
+        wo.Init(sine20Seconds);
+        wo.Play();
+        while (wo.PlaybackState == PlaybackState.Playing)
+        {
+            Thread.Sleep(500);
+        }
+    }
+});
 
 
 using var game = new Lega.Monogame.Shared.Hoopfe.Game1();
